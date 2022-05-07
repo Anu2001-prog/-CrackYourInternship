580715bff64f0b7037a7c652fc21fa072faa5db2
Anuridhi Chhangani <84911349+Anu2001-prog@users.noreply.github.com>
2022-05-08 01:47:57 +0530

    Create DuplicateNumber.java

---
+++ DuplicateNumber.java
@@ -0,0 +1,20 @@
+/*Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.*/
+
+class Solution {
+   public static int findDuplicate(int[] nums) {
+        int len = nums.length;
+        int[] cnt = new int[len + 1];
+        for (int i = 0; i < len; i++) {
+            cnt[nums[i]]++;
+            if (cnt[nums[i]] > 1) {
+                return nums[i];
+            }
+        }
+
+        return len;
+    }
+}
